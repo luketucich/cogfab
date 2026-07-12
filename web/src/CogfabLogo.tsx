38d587eb72ad7532408ae16b1aa0d457ff302d39
luketucich <luketucich@gmail.com>
2026-07-12 04:16:15 -0500

    feat(web): add Cogfab brand identity

---
+++ web/src/CogfabLogo.tsx
@@ -0,0 +1,8 @@
+export function CogfabLogo() {
+  return (
+    <div className="cogfab-brand">
+      <img className="cogfab-brand__mark" src="/brand/cogfab-mark.svg" alt="" aria-hidden="true" />
+      <span className="cogfab-brand__wordmark">Cogfab</span>
+    </div>
+  );
+}
